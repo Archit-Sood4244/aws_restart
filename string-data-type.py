b1136cc96a9bc3ab454059465abab8efdf394ead
Archit-Sood4244 <121092007+Archit-Sood4244@users.noreply.github.com>
2024-01-31 16:35:47 +0530

    Add files via upload

---
+++ string-data-type.py
@@ -0,0 +1,10 @@
+myString="this is a string!!"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+string1="this is "
+string2="a joined string!!!!"
+string3= string1 + string2
+print(string3)
+name = input("What is your name " )
+print(name)
